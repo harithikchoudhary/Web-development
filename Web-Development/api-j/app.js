305658d1c445ccafd8e31eb63bedb1e93deb8507
Harithik Choudhary <harithikchoudhary6@gmail.com>
2025-03-17 09:34:11 +0530

    API

--- Web-Development/api-j/app.js
+++ Web-Development/api-j/app.js
@@ -15,26 +15,70 @@
 // let jsonStudent = JSON.stringify(student);
 // console.log(jsonStudent);
 let url = "https://catfact.ninja/fact";
-fetch(url)
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    console.log(data.fact);
-  })
-  .catch((err) => {
-    console.log("Error", err);
-  });
+// fetch(url)
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.fact);
+//   })
+//   .catch((err) => {
+//     console.log("Error", err);
+//   });
 
 //With async and await
 
+// async function getFacts() {
+//   try {
+//     let res = await fetch(url);
+//     let data = await res.json();
+//     console.log(data.fact);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+  let facts = await getFacts();
+  let p = document.querySelector("#result");
+  p.innerText = facts;
+});
 async function getFacts() {
   try {
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data.fact);
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (e) {
+    console.log(e);
+    return "No Fact Found";
+  }
+}
+
+const jokeUrl = "https://icanhazdadjoke.com/";
+
+async function getJokes() {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    let jokes = await axios.get(jokeUrl, config);
+    console.log(jokes.data.joke);
+    return jokes.data;
+  } catch (e) {
+    console.log(e);
+    return "No Jokes Found";
+  }
+}
+
+let randomUrl = "http://universities.hipolabs.com/search?name=";
+
+let country = "nepal";
+
+async function getColleges() {
+  try {
+    let res = await axios.get(randomUrl + country);
+    console.log(res.data);
   } catch (e) {
     console.log(e);
   }
