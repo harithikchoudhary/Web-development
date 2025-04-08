e1e21fa9ed21980a64928dcd7d2d682460a24edf
Harithik Choudhary <harithikchoudhary6@gmail.com>
2025-04-09 01:54:55 +0530

    Express

---
+++ Web-Development/EXPRESSDIR/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+// console.dir(app);
+
+let port = 8081;
+//Listen incoming api request
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//receiveing a request
+// app.use((req, res) => {
+//   console.log("Request received");
+//   res.send("This is a basic response");
+// });
+
+//sending a response
+// res.send("This is a basic response");
+
+//Routing
+app.get("/home", (req, res) => {
+  res.send("Welcome to home page");
+});
+
+//Nodemon
+//For restarting the server automatically
