76cf96ac2a93a2e4dac61d24570eb29a5e9218d1
Harithik Choudhary <harithikchoudhary6@gmail.com>
2025-04-09 00:53:38 +0530

    Backend

---
+++ Web-Development/Backend/script.js
@@ -0,0 +1,28 @@
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   console.log("Hello", i);
+// }
+
+// let args = process.argv;
+
+// for (let i = 2; i < args.length; i++) {
+//   console.log("Hello to ", args[i]);
+// }
+
+//module.exports
+
+const info = require("./Fruits");
+
+console.log(info);
+
+//require() - is a built in function to include external modules that exist in separate files.
+//module.exports - a special object
+//module.exports = {name: "John", age: 30}; - exports an object with
+
+//NPM (Node Packae Manager)
+//It is a library of packages, or npm is standard package manager for nodejs
+//npm install - install package
+//npm uninstall - uninstall package
+//npm update - update package
+//npm init - create package.json file
